e379df4f819d9753f4e5677fdba74f6281dd7611
Ganz <davidganzhorn@gmail.com>
2010-10-09 15:08:49 -0700

    now with shae

--- game.js
+++ game.js
@@ -28,15 +28,15 @@ Game.prototype.tick = function() {
     var x = -1;
     var y = -1;
     // Occasionally spawn enemies
-    if (this.ticks % 100 == 0) {
+    if (this.ticks % 50 == 0) {
 	if (Math.random() > 0.5) {
 	    // generating on top or bottom
-	    x = (Math.random() > 0.5) ? -20 : 700;
+	    x = (Math.random() > 0.5) ? -100 : 640 + 100;
 	    y = Math.random() * 480;
 	} else {
 	    // generating on left or right
 	    // generating on top or bottom
-	    y = (Math.random() > 0.5) ? -20 : 500;
+	    y = (Math.random() > 0.5) ? -100 : 480 + 100;
 	    x = Math.random() * 640;
 	}
 
@@ -77,9 +77,13 @@ Game.prototype.movePlayer = function() {
     if (this.keyMap[KEYS.D]) this.player.position.x += moveSpeed;
 };
 
-Game.prototype.drawEnemy = function(context, x, y, width) {
+Game.prototype.drawEnemy = function(context, x, y, width, fillColor) {
     context.strokeStyle = "#000000";
-    context.fillStyle = "#333";
+    if (fillColor) {
+	context.fillStyle = fillColor;
+    } else {
+	context.fillStyle = "#333";
+    }
     context.beginPath();
     context.arc(x,y,width,0,Math.PI*2,true);
     context.lineWidth = 2
@@ -117,10 +121,29 @@ Game.prototype.draw = function() {
 
     for (var i = 0; i < this.enemies.length; i++) {
 	var enemy = this.enemies[i];
-	this.drawEnemy(context, enemy.position.x, enemy.position.y, 10);
+	console.info(enemy.position.x, enemy.position.y);
+	if (enemy.position.x < -enemy.size ||
+	    enemy.position.y < -enemy.size ||
+	    enemy.position.x > 640 + enemy.size ||
+	    enemy.position.y > 480 + enemy.size) {
+	    var x = enemy.position.x;
+	    var y = enemy.position.y
+	    if (x < -enemy.size) x = 0;
+	    if (y < -enemy.size) y = 0;
+	    if (x > 640 + enemy.size) x = 640;
+	    if (y > 480 + enemy.size) y = 480;
+	    this.drawEnemy(context, x, y, 10, "#F00");
+	} else {
+	    this.drawEnemy(context, enemy.position.x, enemy.position.y, 10);
+	}
     }
 
     this.drawPlayer(context);
+
+    if (this.gameover) {
+	context.fillStyle = "#FFF";
+	context.fillText("You have shamed ninja kind!", 10, 10);
+    }
 };
 
 Game.prototype.init = function() {
